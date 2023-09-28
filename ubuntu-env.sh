3364efc9fbe3362f89217aac831fc726b0375f6c
chenguojun <nothing>
2023-09-28 14:56:35 +0800

    add ubuntu-env

---
+++ ubuntu-env.sh
@@ -0,0 +1,32 @@
+# replace miirors
+sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+#!/bin/sh
+sudo apt update
+sudo apt install -y vim tmux zsh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-autgsuggestion
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+
+# autojump
+cd 
+git clone https://github.com/wting/autojump.git
+cd autojump
+./install.py
+echo " [[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
+echo "autoload -U compinit && compinit -u" >> ~/.zshrc
+
+rm -rf ~/autojump
+# fzf
+cd 
+git clone https://github.com/junegunn/fzf.git
+cd fzf
+./install
+
+rm -rf ~/fzf
+
+
+
